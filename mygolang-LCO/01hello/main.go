aff75a86709c4f7bef0d188adf40da775a4ec905
SparKling <112405306+krishnaomre@users.noreply.github.com>
2024-08-13 22:14:30 +0530

    Add files via upload

---
+++ mygolang-LCO/01hello/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
+
+func main1() {
+	fmt.Println("hello coders")
+
+}
---
+++ mygolang-LCO/02variable/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var username string = "krishna"
+	fmt.Println(username)
+	fmt.Printf("variable is of type:%T \n", username)
+
+	var isLoggedIn bool = false
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variable is of type:%T \n", isLoggedIn)
+
+	var Smallval int = 255
+	fmt.Println(Smallval)
+	fmt.Printf("variable is of type:%T ", Smallval)
+
+}
